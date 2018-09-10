68af9219015581c08bdfb20ad75ff9dd76a852ca
pierce lally <11771225+PLally@users.noreply.github.com>
2018-09-10 01:09:22 -0400

    added adjustMovementSpeed function

--- lua/autorun/server/sv_pvp_movespeed.lua
+++ lua/autorun/server/sv_pvp_movespeed.lua
@@ -29,11 +29,30 @@ local function playerIsInBuild( ply )
 	return !getPlayerPvpMode( ply )
 end
 
+local function movementMultiplier(weaponCount) 
+    -- calculate movement speed multiplier
+end
+local function adjustMovementSpeed(ply) 
+
+	local weapons = ply:GetWeapons(ply)
+	local wepCount = 0
+	for k, weapon in pairs(weapons) do
+		if nonEffectedWeapons[weapon:GetClass()] == nil then
+            wepCount =  wepCount+1
+            
+        end
+	end
+    multiplier = movementMultiplier(wepCount) 
+    ply:SetRunSpeed(400*multiplier)
+    ply:SetWalkSpeed(200*multiplier) 
+end
 -- Hook Functions --
 local function onEquipped( wep, ply )
 	if not playerIsInBuild( ply ) then return end
+    adjustMovementSpeed( ply )
 end
 
 -- Hooks --
-hook.Remove("WeaponEquip", generateCFCHook("HandleEquipMS"))
-hook.Add("WeaponEquip", generateCFCHook("HandleEquipMS"), onEquipped)
+hook.Remove("WeaponEquip", "HandleEquipMS")
+hook.Add("WeaponEquip", "HandleEquipMS", onEquipped)
+
