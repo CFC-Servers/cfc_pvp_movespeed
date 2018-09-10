981e35f321a078a910824cd3ef4fcf867436d671
Bleck <OverlordBleck@gmail.com>
2018-09-09 22:49:34 -0500

    Initial commit

---
+++ lua/autorun/client/cl_pvp_movespeed.lua
@@ -0,0 +1,2 @@
+-- Includes --
+include( "autorun/shared/sh_pvp_movespeed.lua" )
---
+++ lua/autorun/server/sv_pvp_movespeed.lua
@@ -0,0 +1,3 @@
+-- Includes --
+include( "autorun/shared/sh_pvp_movespeed.lua" )
+AddCSLuaFile( "autorun/shared/sh_pvp_movespeed.lua" )
