0a1812c95aaf496d873c4a8139e2258ecc8045fb
PLally <11771225+PLally@users.noreply.github.com>
2019-01-23 20:18:16 -0500

    changed too many weapons message

--- lua/autorun/server/sv_pvp_movespeed.lua
+++ lua/autorun/server/sv_pvp_movespeed.lua
@@ -49,7 +49,8 @@ end
 
 local function movementMultiplier( totalWeight )
     if totalWeight < 1 then return 1 end
-    return math.Clamp(1 - (1.9^( totalWeight))/100, 0, 1)
+    local multiplier = 1 - ( 1.9 ^ totalWeight  )/ 100
+    return math.Clamp(multiplier, 0, 1)
 end
 
 local function setSpeedFromWeight( ply, totalWeight )
@@ -61,7 +62,7 @@ local function setSpeedFromWeight( ply, totalWeight )
     ply:SetWalkSpeed( math.max( newWalkSpeed, minWalkSpeed ) )
 
     if newWalkSpeed < 100 then
-        ply:ChatPrint("You are holding too many weapons drop some to regain speed")
+        ply:ChatPrint("You are holding too many weapons! Drop some to regain speed.")
         ply:SetCanWalk( false )
     else
         ply:SetCanWalk( true )
