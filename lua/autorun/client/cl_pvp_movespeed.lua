3ff5d791d4f0d13264504512f58a3a6809fac464
Redox <mail@redox.to>
2023-02-18 00:49:42 +0100

    Example linter

--- lua/autorun/client/cl_pvp_movespeed.lua
+++ lua/autorun/client/cl_pvp_movespeed.lua
@@ -2,7 +2,7 @@ concommand.Add( "cfc_dropweapon", function()
     net.Start( "dropPlayerWeapon" )
     net.SendToServer()
 end )
-
+invalid lua example
 concommand.Add( "cfc_dropallweapons", function()
     net.Start( "dropAllWeapons" )
     net.SendToServer()
