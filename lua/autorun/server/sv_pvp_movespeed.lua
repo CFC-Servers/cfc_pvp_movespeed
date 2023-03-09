39762ba3c464c8bade930e317330225b2ff71a60
GenericRicochetPlayer <113946528+GenericRicochetPlayer@users.noreply.github.com>
2023-03-09 17:04:33 -0500

    Update sv_pvp_movespeed.lua

--- lua/autorun/server/sv_pvp_movespeed.lua
+++ lua/autorun/server/sv_pvp_movespeed.lua
@@ -22,7 +22,6 @@ local weaponWeights = {
     m9k_m60  = 3,
     m9k_pkm  = 3,
     m9k_m1918bar  = 3,
-    m9k_fg42  = 3,
     m9k_m249lmg  = 3,
     m9k_ares_shrike  = 3,
     weapon_lfsmissilelauncher = 4,
