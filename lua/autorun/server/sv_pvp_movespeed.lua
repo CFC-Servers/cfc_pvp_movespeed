937fb0332694e22bcd9042bf6081fe77ca5f7bfc
Pierce Lally <pierce@vulpes.dev>
2023-11-07 12:34:21 -0500

    Update sv_pvp_movespeed.lua (#39)

--- lua/autorun/server/sv_pvp_movespeed.lua
+++ lua/autorun/server/sv_pvp_movespeed.lua
@@ -18,13 +18,7 @@ local weaponWeights = {
     m9k_m202          = 3,
     m9k_rpg7          = 3,
     m9k_minigun       = 6,
-    cw_m249_official  = 3,
-    m9k_m60  = 3,
-    m9k_pkm  = 3,
-    m9k_m1918bar  = 3,
-    m9k_m249lmg  = 3,
     tfa_l4d2mw_riotshield = 3,
-    m9k_ares_shrike  = 3,
     weapon_lfsmissilelauncher = 4,
 }
 pvpMoveSpeed = {}
